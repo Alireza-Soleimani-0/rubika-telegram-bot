dacee30165729280cada17f94714bcb608318145
Alireza-Soleimani <165762170+Alireza-Soleimani-0@users.noreply.github.com>
2026-02-22 01:58:54 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,53 @@
-import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from rubpy import Client
 
-TOKEN = os.getenv("BOT_TOKEN")
+sessions = {}
 
-if not TOKEN:
-    raise ValueError("BOT_TOKEN is not set!")
+async def start(message):
+    user_id = message.author_guid
+    sessions[user_id] = {"step": "phone"}
+    await message.reply("📱 شماره تلفن را ارسال کنید\nمثال: 09123456789")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("ربات روشنه ✅")
 
-def main():
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    print("Bot is running...")
-    app.run_polling()
+async def handler(message):
+    user_id = message.author_guid
 
-if __name__ == "__main__":
-    main()
+    if user_id not in sessions:
+        return
+
+    step = sessions[user_id]["step"]
+
+    # مرحله دریافت شماره
+    if step == "phone":
+        phone = message.text.strip()
+        sessions[user_id]["phone"] = phone
+
+        client = Client(phone_number=phone)
+        await client.connect()
+
+        sent = await client.send_code(phone)
+
+        sessions[user_id]["client"] = client
+        sessions[user_id]["phone_code_hash"] = sent.phone_code_hash
+        sessions[user_id]["step"] = "code"
+
+        await message.reply("📩 کد تایید ارسال شد، کد را بفرستید:")
+
+    # مرحله دریافت کد
+    elif step == "code":
+        code = message.text.strip()
+
+        client = sessions[user_id]["client"]
+        phone = sessions[user_id]["phone"]
+        hash_ = sessions[user_id]["phone_code_hash"]
+
+        try:
+            await client.sign_in(phone, hash_, code)
+
+            # ذخیره سشن
+            await client.save_session()
+
+            sessions[user_id]["step"] = "done"
+            await message.reply("✅ لاگین انجام شد")
+
+        except Exception as e:
+            await message.reply(f"❌ خطا در لاگین: {e}")
